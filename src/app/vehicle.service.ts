4cfb8720c4853de5baacf6cc9db0b957dbd61eb2
Ravikanikireddy <dombaraashok@gmail.com>
2023-07-23 23:57:13 +0530

    view,edit,update in vehicle servise change

--- src/app/vehicle.service.ts
+++ src/app/vehicle.service.ts
@@ -11,8 +11,8 @@ export class VehicleService {
   getVehicles(): Observable<any> {
     return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction")
   }
-  getVehicle(id:any):Observable<any>{
- return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/"+id)
+  getVehicle(id: any): Observable<any> {
+    return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/" + id)
   }
   getFilteredVehicles(term: any): Observable<any> {
     return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?filter=" + term)
@@ -23,11 +23,11 @@ export class VehicleService {
   getPageVehicles(limit: any, page: any): Observable<any> {
     return this.httpClient.get("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction?limit=" + limit + "&page=" + page)
   }
-  createVehicles(data:any) {
-     return this.httpClient.post("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction",data)
+  createVehicles(data: any): Observable<any> {
+    return this.httpClient.post("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction", data)
   }
-  updateVehicles(id:any,data:any){
-    return this.httpClient.put("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/"+id,data)
+  updateVehicles(id: any, data: any): Observable<any> {
+    return this.httpClient.put("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/" + id, data)
   }
   deleteVehicles(id: any): Observable<any> {
     return this.httpClient.delete("https://6128991386a213001729f9df.mockapi.io/test/v1/jurisdiction/" + id)

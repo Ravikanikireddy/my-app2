65feb583e16f20aded172a6e47ef380a0858188b
Ravikanikireddy <dombaraashok@gmail.com>
2023-07-26 11:26:18 +0530

    1,2.event creation to emit from child component

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,5 +6,11 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./child.component.scss']
 })
 export class ChildComponent {
- @Input() public ac:number = 0;
+  @Input() public ac: number = 0;
+  public bc: number = 0;
+  @Output() public bEvent:EventEmitter<any> = new EventEmitter();
+
+  send(){
+    this.bEvent.emit(this.bc);
+  }
 }
